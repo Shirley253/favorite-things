88f695b86367f075648915b2352017def79ac4df
Brian Henry and Shirley Dennis <shirley@>
2018-03-26 13:35:57 -0700

    Add list items and send form info to the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
 $(document).ready(function() {
-  $(formOne).submit(function() {
-    var favFoods = $("#favFood").val();
-    var favoriteVacation = $("#favoriteVacation").val();
-    var favoriteColor = $("#favoriteColor").val;
-  })
-})
-function pushToArrays(favFoods, favoriteVacation, favoriteColor){
-  favFoods.push(Food);
-  favoriteVacation.push(Vacation);
-  favoriteColor.push(Color);
 
   $("#formOne").submit(function(event) {
     var favoriteThings = ["#favFood", "#favoriteVacation", "#favoriteColor"];
-
-    blanks.forEach(function(blank) {
-      var userInput = $("input." + blank).val();
-      console.log(userInput);
-      $("." + blank).text(userInput).val();
-    });
-    $("#story").show();
+    var vacationInput = $("input#favoriteVacation").val();
+    var colorInput = $("input#favoriteColor").val();
+    var foodInput = $("input#favFood").val();
+    // favoriteThings.forEach(function(blank) {
+    //   var userInput = $("input" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
+    console.log(favoriteThings);
+          // favoriteColor.push(Color);
+    // form = new Array();
     event.preventDefault();
+
+    // append
+    $(".food-result").append(foodInput);
+    $(".vacation-result").append(vacationInput);
+    $(".color-result").append(colorInput);
+
+    $(".result").show();
+
   });
+
 });
 
-$(function()}
-  $(".button").click(function() {
-  // $("p").addClass(favoriteThings);
+//   $(formOne).submit(function() {
+//     var favFoods = $("#favFood").val();
+//     var favoriteVacation = $("#favoriteVacation").val();
+//     var favoriteColor = $("#favoriteColor").val();
+//     function pushToArrays(favFoods, favoriteVacation, favoriteColor);{
+//       favFoods.push(Food);
+//       favoriteVacation.push(Vacation);
+
+//
+//   });
+// });
